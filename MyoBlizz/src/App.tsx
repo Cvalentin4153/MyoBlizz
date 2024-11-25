3a64ae0b2977c03d5420b285e4ecf8d9b253d230
cvalentin4153 <cvalentin4153@outlook.com>
2024-11-25 19:40:01 -0400

    Nav/Logo ADD

--- MyoBlizz/src/App.tsx
+++ MyoBlizz/src/App.tsx
@@ -1,26 +1,29 @@
-import OverlayImage from "./Image/bgIM.png";
-
+import OverlayImage from "./Image/bgIM.svg";
+import NavLogo from "./Image/NavLogo.png";
 function App() {
   return (
-    <main
-      style={{ backgroundImage: `url(${OverlayImage})` }}
-      className="h-screen bg bg-gradient-to-b from-black to-stone-400 bg-blend-overlay bg-cover"
-    >
-      <h1
-        className="text-9xl font-bold text- py-10 text-center bg-gradient-to-b from-cyan-600 via-green-400 to-blue-900 text-transparent bg-clip-text max-h-fit"
-        id="title"
+    <>
+      <div
+        style={{ backgroundImage: `url(${OverlayImage})` }}
+        className="h-screen bg-cover bg-no-repeat bg-center"
       >
-        MyoBlizz
-      </h1>
-      <div className="mx-auto flex items-center justify-center">
-        <button
-          class="px-8 py-2 absolute bottom-10 mb-11 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-xl font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-600 to-blue-700 group-hover:from-green-800 group-hover:to-blue-900 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:bg-green-900"
-          id="ShopButton"
-        >
-          Shop Now
-        </button>
+        <header className="flex justify-between items-center py-2 bg-neutral-900 drop-shadow-2xl shadow-neutral-600">
+          <a href="#">
+            <img
+              src={NavLogo}
+              alt=""
+              className="w-[147px] hover:scale-90 transition-all float-left"
+            />
+          </a>
+          <ul className="hidden xl:flex justify-center items-center gap-12 font-semibold text-white font-serif">
+            <li>About</li>
+            <li>Products</li>
+            <li>Collections</li>
+            <li className="bx bx-cart"></li>
+          </ul>
+        </header>
       </div>
-    </main>
+    </>
   );
 }
 
