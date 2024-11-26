f607f897e7407d5c7f47f4887cefc0fea0c9dc9e
cvalentin4153 <cvalentin4153@outlook.com>
2024-11-26 09:16:27 -0400

    Quick Fix

--- MyoBlizz/src/App.tsx
+++ MyoBlizz/src/App.tsx
@@ -15,9 +15,9 @@ function App() {
               className="w-[210px] hover:scale-90 transition-all float-left ml-8"
             />
           </a>
-          <ul className="hidden xl:flex justify-center items-center text-center ml-19 -mr-6 gap-24 font-medium font-roboto text-white text-lg">
+          <ul className="flex justify-center items-center text-center ml-19 -mr-6 gap-24 font-medium font-roboto text-white text-lg w-full h-full relative">
             <li>
-              <button className="hover:scale-105 transition-all bg-stone-800 bg-opacity-80 border-none rounded-full py-1 px-4 flex-row justify-center align-middle text-center">
+              <button className="hover:scale-105 transition-all bg-stone-800 bg-opacity-80 border-none rounded-full py-1 px-4 flex-row justify-center align-middle text-center block">
                 About
               </button>
             </li>
@@ -27,15 +27,17 @@ function App() {
               </button>
             </li>
             <li>
-              <button className="hover:scale-105 transition-all bg-stone-800 bg-opacity-80 border-none rounded-full py-1 px-4 flex-row justify-center align-middle text-center">
+              <button className="hover:scale-105 transition-all bg-stone-800 bg-opacity-80 border-none rounded-full py-1 px-4 flex-row justify-center items-center text-center block">
                 Collections
               </button>
             </li>
           </ul>
-          <div className="flex justify-center items-center align-middle gap-y-0.5">
-            <button className="bg-stone-800 bg-opacity-80 mr-8 flex justify-center items-center py-1 px-4 border-none rounded-full hover:scale-90">
+          <div className="flex justify-center items-center">
+            <button className="bg-stone-800 bg-opacity-80 mr-8 gap-y-0.5 flex justify-center items-center py-0 px-4 border-none rounded-full hover:scale-90">
               <i className="bx bx-cart bx-sm bx-tada-hover mr-2 text-white" />
-              <h3 className="text-white font-roboto">View Cart</h3>
+              <h3 className="text-white font-roboto flex-row justify-center items-center ">
+                View Cart
+              </h3>
             </button>
           </div>
         </header>
